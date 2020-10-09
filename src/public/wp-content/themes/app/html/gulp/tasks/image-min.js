663129f9d6baac7774444fbd789bbd22f5bf7126
Vital Ozierski <v.ozierski@ozitag.com>
2020-10-09 22:07:40 +0300

    upload meta

--- src/public/wp-content/themes/app/html/gulp/tasks/image-min.js
+++ src/public/wp-content/themes/app/html/gulp/tasks/image-min.js
@@ -1,6 +1,6 @@
 module.exports = () => {
   $.gulp.task('imageMin', () => {
-    if (!$.config.imageMin) return false;
+    if (!$.config.imageMin) return $.gulp.src('.').pipe($.nop());
     return $.gulp.src(`${$.config.outputPath}/${$.config.imagesPath}/**/*.{png,jpg,gif}`)
     .pipe($.gulpPlugin.imagemin({
       interlaced: true,
