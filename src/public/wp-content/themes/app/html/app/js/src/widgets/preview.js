7d6813f7a82cb93eef11dbab63ea5cf4b3731ab5
Mikle <m.pietrovski@ozitag.com>
2020-10-28 13:44:05 +0300

    Update home

---
+++ src/public/wp-content/themes/app/html/app/js/src/widgets/preview.js
@@ -0,0 +1,42 @@
+class Preview {
+  constructor(item) {
+    this.preview = item;
+    this.addEvents();
+  }
+
+  addEvents() {
+    this.preview.addEventListener('mouseover', () => {
+      if (this.preview.classList.contains('active')) {
+        return;
+      } else {
+        const container = this.preview.closest('.js-previews');
+        const currActive = container.querySelector('.active');
+
+        if (currActive) {
+          currActive.classList.remove('active');
+        }
+
+        this.setActive(this.preview);
+      }
+    });
+  }
+
+  setActive(elem) {
+    elem.classList.add('active');
+  }
+
+  removeActive(elem) {
+    elem.classList.remove('active');
+  }
+
+  static init(elem) {
+    new Preview(elem);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preview = document.querySelectorAll('.js-preview');
+  preview.forEach(item => {
+    Preview.init(item);
+  });
+});
