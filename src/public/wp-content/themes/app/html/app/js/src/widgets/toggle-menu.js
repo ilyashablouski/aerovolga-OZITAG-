b0d3571447d24489b626bea875ae1c5633f36455
Mikle <m.pietrovski@ozitag.com>
2020-11-09 12:15:16 +0300

    Add hover from footer

--- src/public/wp-content/themes/app/html/app/js/src/widgets/toggle-menu.js
+++ src/public/wp-content/themes/app/html/app/js/src/widgets/toggle-menu.js
@@ -9,16 +9,25 @@ class ToggleMenu {
   addEvents() {
     this.toggleMenu.addEventListener('click', () => {
 
+
       if (this.toggleMenu.classList.contains('active')) {
         this.removeActive(this.toggleMenu);
         this.removeActive(this.menu);
-
         document.body.style.overflow = 'auto';
+
+        if (window.innerWidth < 1024) {
+          this.removeActive(this.menu.parentNode);
+          this.menu.parentNode.style.animation = 'mobile-hidden 0.5s linear';
+        }
       } else {
         this.setActive(this.toggleMenu);
         this.setActive(this.menu);
-
         document.body.style.overflow = 'hidden';
+
+        if (window.innerWidth < 1024) {
+          this.setActive(this.menu.parentNode);
+          this.menu.parentNode.style.animation = 'none';
+        }
       }
     });
   }
