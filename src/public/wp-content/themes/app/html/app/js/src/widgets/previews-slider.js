c45bc24898c0559ee8b61c8534866371234edca4
galko <v.galko@agentestudio.com>
2020-11-24 00:42:11 +0300

    enable slideToClickedSlide for features slider

--- src/public/wp-content/themes/app/html/app/js/src/widgets/previews-slider.js
+++ src/public/wp-content/themes/app/html/app/js/src/widgets/previews-slider.js
@@ -12,11 +12,6 @@ const configs = {
     bulletActiveClass: 'active',
     clickable: true,
   },
-  cubeEffect: {
-    slideShadows: false,
-    shadow: false,
-  },
-
 };
 
 class PreviewsSlider {
