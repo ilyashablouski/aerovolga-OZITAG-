e1cc5fa93a088e9321ffd07d0854eed6d24fa392
Mikle <m.pietrovski@ozitag.com>
2020-12-11 16:38:04 +0300

    update slide

--- src/public/wp-content/themes/app/html/app/js/src/widgets/reviews-slider.js
+++ src/public/wp-content/themes/app/html/app/js/src/widgets/reviews-slider.js
@@ -1,7 +1,7 @@
 class ReviewsSlider {
+
   queryElements(componentName) {
     const element = this.nodeElement.querySelectorAll('.js-reviews-slider__' + componentName);
-
     if (!element) {
       console.warn(`JS Double Slider - ${componentName} not found`);
     }
@@ -13,17 +13,17 @@ class ReviewsSlider {
     this.nodeElement = nodeElement;
 
     this.rowElement = this.queryElements('row');
-
     this.initCertificateSlider();
   }
 
   initCertificateSlider() {
     this.initSlider();
+    this.updateCurrentButton();
+    this.bindEvents();
   }
 
   initSlider() {
 this.rowElement.forEach( (item)=> {
-  console.log(item.getAttribute('data-thumbs'));
   if(item.getAttribute('data-thumbs')) {
     this.slider = new Swiper(item, {
       speed: 1000,
@@ -33,7 +33,6 @@ this.rowElement.forEach( (item)=> {
         nextEl: '.thumbs-slide__next',
         prevEl: '.thumbs-slide__prev',
       },
-
     });
 
   } else {
@@ -48,6 +47,52 @@ this.rowElement.forEach( (item)=> {
     });
   }
 })
+  }
+
+  goNext() {
+    this.slider.slideNext();
+  }
+
+  onNextClick(e) {
+    e.preventDefault();
+    this.goNext();
+  }
+
+  goPrev() {
+    this.slider.slidePrev();
+  }
+
+  onPrevClick(e) {
+    e.preventDefault();
+    this.goPrev();
+  }
+
+  updateCurrentButton() {
+    const nextElement =  this.nodeElement.querySelector('.reviews__thumbs-slide.swiper-slide-next');
+    const prevElement =  this.nodeElement.querySelector('.reviews__thumbs-slide.swiper-slide-prev');
+    if (nextElement) {
+      nextElement.addEventListener('click', this.onNextClick.bind(this));
+    }
+    if (prevElement) {
+      prevElement.addEventListener('click', this.onPrevClick.bind(this));
+    }
+  }
+
+  resetCurrentButton() {
+    this.nodeElement.querySelectorAll('.reviews__thumbs-slide').forEach((item)=>{
+      item.removeEventListener('click', ()=> {
+        return false;
+      })
+    })
+  }
+
+  onSwiperSlideChange() {
+    this.resetCurrentButton()
+    this.updateCurrentButton();
+  }
+
+  bindEvents() {
+    this.slider.on('slideChange', this.onSwiperSlideChange.bind(this));
   }
 
   static init(elem) {
