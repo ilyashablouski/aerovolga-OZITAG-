32ba9c8256c6a27a4a3e2a9d9bd25df29f1bcd0a
Mikle <m.pietrovski@ozitag.com>
2020-12-11 17:13:53 +0300

    update scroll

--- src/public/wp-content/themes/app/html/app/js/src/widgets/reviews-slider.js
+++ src/public/wp-content/themes/app/html/app/js/src/widgets/reviews-slider.js
@@ -18,8 +18,6 @@ class ReviewsSlider {
 
   initCertificateSlider() {
     this.initSlider();
-    this.updateCurrentButton();
-    this.bindEvents();
   }
 
   initSlider() {
@@ -49,63 +47,11 @@ this.rowElement.forEach( (item)=> {
 })
   }
 
-  goNext() {
-    this.slider.slideNext();
-  }
-
-  onNextClick(e) {
-    e.preventDefault();
-    this.goNext();
-  }
-
-  goPrev() {
-    this.slider.slidePrev();
-  }
-
-  onPrevClick(e) {
-    e.preventDefault();
-    this.goPrev();
-  }
-
-  updateCurrentButton() {
-    const nextElement =  this.nodeElement.querySelector('.reviews__thumbs-slide.swiper-slide-next');
-    const prevElement =  this.nodeElement.querySelector('.reviews__thumbs-slide.swiper-slide-prev');
-    if (nextElement) {
-      nextElement.addEventListener('click', this.onNextClick.bind(this));
-    }
-    if (prevElement) {
-      prevElement.addEventListener('click', this.onPrevClick.bind(this));
-    }
-  }
-
-  resetCurrentButton() {
-    this.nodeElement.querySelectorAll('.reviews__thumbs-slide').forEach((item)=>{
-      item.removeEventListener('click', ()=> {
-        return false;
-      })
-    })
-  }
-
-  onSwiperSlideChange() {
-    this.resetCurrentButton()
-    this.updateCurrentButton();
-  }
-
-  bindEvents() {
-    this.slider.on('slideChange', this.onSwiperSlideChange.bind(this));
-  }
 
   static init(elem) {
     new ReviewsSlider(elem);
   }
 }
-
-/* document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelectorAll('.js-reviews-slider');
-  slider.forEach(item => {
-    ReviewsSlider.init(item);
-  });
-}); */
 
 subscribeToEvent('initModules', () => {
   const slider = document.querySelectorAll('.js-reviews-slider');
