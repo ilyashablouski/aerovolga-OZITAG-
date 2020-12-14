c97553e78ed6031230263987abf7a2c307a09555
Mikle <m.pietrovski@ozitag.com>
2020-12-14 11:35:52 +0300

    Update media

--- src/public/wp-content/themes/app/html/app/js/src/widgets/gallery-slider.js
+++ src/public/wp-content/themes/app/html/app/js/src/widgets/gallery-slider.js
@@ -1,28 +1,3 @@
-const configs = {
-  speed: 600,
-  autoplay: {
-    delay: 2000,
-  },
-  navigation: {
-    nextEl: '.gallery-slider__next',
-    prevEl: '.gallery-slider__prev'
-  },
-  pagination: {
-    el: '.gallery-slider__pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-  effect: 'coverflow',
-  coverflowEffect: {
-    slideShadows: false,
-  },
-  cubeEffect: {
-    slideShadows: false,
-    shadow: false,
-  },
-
-};
-
 class GallerySlider {
   constructor(nodeElement) {
     this.nodeElement = nodeElement;
@@ -35,20 +10,33 @@ class GallerySlider {
   }
 
   initSlider() {
-    this.swiper = new Swiper(this.nodeElement, configs);
+    this.swiper = new Swiper(this.nodeElement, {
+      speed: 600,
+      navigation: {
+        nextEl: '.gallery-slider__next',
+        prevEl: '.gallery-slider__prev',
+      },
+      pagination: {
+        el: '.gallery-slider__pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+      effect: 'coverflow',
+      coverflowEffect: {
+        slideShadows: false,
+      },
+      cubeEffect: {
+        slideShadows: false,
+        shadow: false,
+      },
+
+    });
   }
 
   static init(elem) {
     new GallerySlider(elem);
   }
 }
-
-/* document.addEventListener('DOMContentLoaded', () => {
-  const slider = document.querySelectorAll('.js-gallery-slider');
-  slider.forEach(item => {
-    GallerySlider.init(item);
-  });
-}); */
 
 subscribeToEvent('initModules', () => {
   const slider = document.querySelectorAll('.js-gallery-slider');
